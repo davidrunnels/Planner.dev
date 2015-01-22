1817c303712e5e5c3d390426e3bf622558bab576
David Runnels <david.runnels@gmail.com>
2015-01-22 08:17:59 -0600

    add filestore and addressDataStore

--- public/address_book.php
+++ public/address_book.php
@@ -1,167 +1,114 @@
 <?php
 
-class AddressDataStore
-{
-	public $filename = '';
+require_once '../inc/address_data_store.php';
 
-	function __construct($filename = 'address_book.csv')
+$error_message = [];
+$fields = [];
+
+	$address_object = new AddressDataStore("address_book.csv");
+
+	// var_dump($address_object);
+
+	$addressBook = $address_object->read();
+
+	if (count($_FILES) > 0 && $_FILES['file1']['error'] == UPLOAD_ERR_OK) 
 	{
-		$this->filename = $filename;
-	}
+		// Set the destination directory for uploads
+		$uploadDir = '/vagrant/sites/planner.dev/public/uploads/';
 
-	function readAddressBook()
-	{
-		$addressBook = [];
-         // Code to read file $this->filename
-		if (($handle = fopen($this->filename, "r")) !== FALSE) {
-			while(!feof($handle)) {
-				$row = fgetcsv($handle);
+		// Grab the filename from the uploaded file by using basename
+		$filename = basename($_FILES['file1']['name']);
 
-				if (!empty($row)) {
-					$addressBook[] = $row;
-				}
-			}
 
-			fclose($handle);
+		if (substr($filename, -3) == "csv") {
+
+			// Create the saved filename using the file's original name and our upload directory
+			$savedFilename = $uploadDir . $filename;
+
+			// Move the file from the temp location to our uploads directory
+			move_uploaded_file($_FILES['file1']['tmp_name'], $savedFilename);
+
+			$address_new_file = new AddressDataStore($savedFilename);
+
+			$address_array_uploads = $address_object->read();
+			$addressBook = array_merge($addressBook, $address_array_uploads);
+			$address_object->write($addressBook);
+
+		} else {
+			throw new Exception("File is not a csv file.");
 		}
-
-		return $addressBook;
 	}
-
-	function writeAddressBook($addressesArray)
-	{
-		// var_dump($addressesArray);
-         // Code to write $addressesArray to file $this->filename
-		$handle = fopen($this->filename, 'w');
-			// var_dump($array);
-		foreach ($addressesArray as $fields) {
-			fputcsv($handle, $fields);
-		}
-		fclose($handle);
-	}
-
-		// $fileName = 'address_book.csv';
-
-
-	function sanitize($array) {
-		foreach ($array as $key => $value) {
-        		$array[$key] = htmlspecialchars(strip_tags($value));  // Overwrite each value.
-        	}
-        	return $array;
-        }
-
-  //   function opencsv()
-  //   {
-		// $contentsarray = [];
-		// if(filesize($this->filename) != 0) {
-		// 	$handle = fopen($this->filename, 'r');
-		// 	$contents = trim(fread($handle, filesize($this->filename)));
-		// 	$contentsarray = explode("\n", $contents);
-		// 	fclose($handle);
-
-		// 	var_dump($contentsarray);
-		// } 
-
-		// return $contentsarray;
-
-// 	}
-    }
-
-    $address_object = new AddressDataStore();
-
-    $address_object->filename = "address_book.csv";
-
-    // var_dump($address_book);
-
-    $addressBook = $address_object->readAddressBook();
-
-    if (count($_FILES) > 0 && $_FILES['file1']['error'] == UPLOAD_ERR_OK) 
-    {
-    // Set the destination directory for uploads
-    	$uploadDir = '/vagrant/sites/planner.dev/public/uploads/';
-
-    // Grab the filename from the uploaded file by using basename
-    	$filename = basename($_FILES['file1']['name']);
-
-
-    	if (substr($filename, -3) == "csv") {
-
-    // Create the saved filename using the file's original name and our upload directory
-    		$savedFilename = $uploadDir . $filename;
-
-    // Move the file from the temp location to our uploads directory
-    		move_uploaded_file($_FILES['file1']['tmp_name'], $savedFilename);
-
-    		$address_new_file = new AddressDataStore($savedFilename);
-
-    		$address_array_uploads = $address_object->readAddressBook();
-    		$addressBook = array_merge($addressBook, $address_array_uploads);
-    		$address_object->writeAddressBook($addressBook);
-
-    	} else {
-    		echo "File is not a csv file.";
-    	}
-    }
 
 
 	// print_r($addressBook);
 
 	//Add data from form to $addressBook array.
 
-    if (!empty($_POST)) {
+	if (!empty($_POST)) {
 	//work with post data
 	// var_dump($_POST);
-    	$_POST = $address_object->sanitize($_POST);
-    	foreach ($_POST as $key => $value) {
-    		if (!empty($value)) {
-    			$fields[] = $value;
-    		} else {
-    			echo ("<p>You must complete {$key} field.</p>");
-    		}
-    	}
+		$_POST = $address_object->sanitize($_POST);
+		foreach ($_POST as $key => $value) {
+			if (!empty($value)) {
+				$fields[] = $value;
+			} else {
+
+				try {
+
+					$address_object->validateAddressbookInput($value);
+				}
+				
+				catch (InvalidAddressbookInputException $e) {
+					echo $e->getMessage();
+					throw new Exception("<p>You must complete {$key} field.</p>");
+				}
+			}
+		}
 
 	// $fields = [ $_POST['name'], $_POST['street'], $_POST['city'], $_POST['state'], $_POST['zip'], $_POST['phone'] ];
 	// var_dump($fields);
 
 	// add to array
 
-    	array_push($addressBook, $fields);
+		array_push($addressBook, $fields);
 	// var_dump($addressBook);
- 	// save to file
-    	$address_object->writeAddressBook($addressBook);
-    }
+	// save to file
+		$address_object->write($addressBook);
+	}
 
 // var_dump($addressBook);
 
-    if(isset($_GET['remove'])) {
-    	$key = $_GET['remove'];
-    	unset($addressBook[$key]);
-    	$addressBook = array_values($addressBook);
-    	$address_object->writeAddressBook($addressBook);
-    }
-    ?>
+	if(isset($_GET['remove'])) {
+		$key = $_GET['remove'];
+		unset($addressBook[$key]);
+		$addressBook = array_values($addressBook);
+		$address_object->write($addressBook);
+	}
 
-    <html>
-    <head>
-    	<title>Address Book</title>
-    	<link href="/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	unset($address_object);
+	?>
 
-    	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-      <![endif]-->
+	<html>
+	<head>
+		<title>Address Book</title>
+		<link href="/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-      <!-- Font Awesome -->
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+	  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	  <![endif]-->
 
-      <link rel="stylesheet" href="/font-awesome-4.2.0/css/font-awesome.min.css">
+	  <!-- Font Awesome -->
 
-      <!-- Padding -->
-     <!-- <style type="text/css">
-      body { 
-        padding-top: 70px; 
-      }
+	  <link rel="stylesheet" href="/font-awesome-4.2.0/css/font-awesome.min.css">
+
+	  <!-- Padding -->
+	 <!-- <style type="text/css">
+	  body { 
+		padding-top: 70px; 
+	  }
   </style> -->
 
   <link rel="stylesheet" href="/css/sites.css">
@@ -190,7 +137,9 @@ class AddressDataStore
 				</tr>
 				<!-- Start working with php, and echoing out the data from $addressBook -->
 				<tr>
+					
 					<? foreach ($addressBook as $key => $fields) : ?>
+
 					<tr>
 						<? foreach ($fields as $value) : ?>
 						<td><?= $value; ?></td>
